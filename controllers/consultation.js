b7c0c309de8d56ccfc82ee32aef6cd5719d914de
igbdharmaputra <dharmatkjone@gmail.com>
2021-01-18 02:53:24 +0800

    hallo

---
+++ controllers/consultation.js
@@ -0,0 +1,65 @@
+const { consultation, user } = require("../models");
+
+exports.consul = async (req, res) => {
+  try {
+    req.body.userId = req.user;
+
+    console.log(req.role);
+
+    if (req.role === 0) {
+      const newConsul = await consultation.create(req.body);
+      const consul = await consultation.findOne({
+        where: { id: newConsul.id }
+      });
+      res.status(200).send({ data: consul });
+    } else {
+      res.status(405).send({ message: "Failure" });
+    }
+  } catch (error) {
+    res.status(500).send({ message: "Unknown Request" });
+    console.log(error);
+  }
+};
+
+exports.getconsul = async (req, res) => {
+  try {
+    const consul = await consultation.findOne({
+      where: { id: req.params.id }
+    });
+    res.status(200).send({ data: consul });
+  } catch (error) {
+    res.status(500).send({ message: "Unknown Request" });
+    console.log(error);
+  }
+};
+
+exports.getAll = async (req, res) => {
+  try {
+    if (req.role === 0) {
+      const consul = await consultation.findAll({
+        where: { userId: req.user }
+      });
+      res.status(200).send({ data: consul });
+    } else {
+      const consul = await consultation.findAll();
+      res.status(200).send({ data: consul });
+    }
+  } catch (error) {
+    res.status(500).send({ message: "Unknown Request" });
+    console.log(error);
+  }
+};
+
+exports.reply = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const consul = await consultation.findAll({
+      where: { userId: req.user }
+    });
+    res.status(200).send({ data: consul });
+  } catch (error) {
+    res.status(500).send({ message: "Unknown Request" });
+    console.log(error);
+  }
+};
---
+++ models/consultation.js
@@ -0,0 +1,27 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  const consultation = sequelize.define(
+    "consultation",
+    {
+      fullName: DataTypes.STRING,
+      phone: DataTypes.STRING,
+      bornDate: DataTypes.STRING,
+      age: DataTypes.STRING,
+      height: DataTypes.STRING,
+      weight: DataTypes.STRING,
+      gender: DataTypes.STRING,
+      subject: DataTypes.STRING,
+      liveConsul: DataTypes.STRING,
+      description: DataTypes.STRING,
+      status: DataTypes.STRING,
+      userId: DataTypes.INTEGER,
+      dokterId: DataTypes.INTEGER
+    },
+    {}
+  );
+  consultation.associate = function(models) {
+    consultation.belongsTo(models.user);
+    consultation.hasOne(models.reply);
+  };
+  return consultation;
+};
